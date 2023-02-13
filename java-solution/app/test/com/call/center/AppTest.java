8c9557f5c637b614dba9363e9bbb20e10a69906a
ccarcaci <claudiocarcaci@gmail.com>
2023-02-13 18:59:05 +0100

    Fast push

--- java-solution/app/test/com/call/center/AppTest.java
+++ java-solution/app/test/com/call/center/AppTest.java
@@ -5,11 +5,12 @@ package com.call.center;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+  @Test
+  void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+  }
 }
